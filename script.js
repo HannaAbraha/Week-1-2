2f118ac7813c8337ba1d13d23d5d86e30e336900
Hanna Abraha <Scohan6@yahoo.com>
2023-01-05 13:42:20 -0800

    second command

---
+++ script.js
@@ -0,0 +1,46 @@
+//print hello
+console.log("hello");
+var myStr = "hello";
+//variable named x eqial to 5
+var x = 5;
+
+//print variable x
+console.log("x:",x);
+
+//reassign x to 7
+x=7;
+//output x
+console.log("X:",x);
+
+//create a var named y equal to 3
+var y = 3;
+//create a var named z = x + y
+var z = x + y; //value is 10
+
+console.log(z);
+
+var a = myStr + x;
+console.log(a);
+
+var b = x - y; //4
+console.log(b)
+
+b = x/y;
+console.log(b)
+
+b = x * y;
+console.log(b);
+
+b++; //b = b + 1
+console.log(b);
+
+--b; //b = b - 1
+console.log(b);
+
+
+console.log(b % 3);
+
+console.log(b % 2);
+
+b = b % 2;
+console.log(b);
